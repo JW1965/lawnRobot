ed24cfcdc200f0897ccccf535132c755b7db2e86
jensw <jensw@Saopaulo.fritz.box>
2018-12-25 23:44:46 +0100

    c1

---
+++ lawnRobot/src/robot/robot.java
@@ -0,0 +1,25 @@
+package robot;
+
+public class robot {
+	
+	// methods
+	
+	// public static int move()
+	// requests lawn mover to do the next move
+	// the result indicates the angle of the move and must be one of 
+	//    0, 45, 90, 135, 180, 225, 270, 315
+	
+	// public static boolean isFinsihed ()
+	// return true if robot indicates that the job is done
+	// before the first step, the result ist false
+	
+	// public static boolean cantMove ()
+	// indicates that the robot stopped working without finishing
+	// this is used for later extensions like out of battery status
+	// before the first step, the result ist false
+		
+	// public static boolean sensorTriggered ()
+	// false if last move was successful
+	// true if lawn mover hits an obstacle or fence and move was not perfomed
+
+}
